c1b5e9f5b7ee1f50c4d6fb6657efd71f97d7b83d
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2022-10-28 20:34:06 +0530

    Time: 344 ms (61.48%), Space: 77.2 MB (67.77%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,42 +1,29 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size(), res = 0;
+        int n = heights.size(), res = 0, i = 0;
         
-        // Left boundary for element
-        vector<int> leftSmall(n, 0);
         stack<int> st;
         
-        for(int i = 0; i < n; ++i) {
-            // Calculate area for greater elements
-            // and remove them from stack
-            while(!st.empty() && heights[st.top()] > heights[i]) {
+        while(i < n || !st.empty()) {
+            // Calculate area for greater elements and remove them from stack
+            while(
+                !st.empty() &&
+                ((i < n && heights[st.top()] > heights[i]) ||
+                (i == n && !st.empty()))
+            ) {
                 int idx = st.top();
-                int lB = leftSmall[idx], rB = i-1;
+                st.pop();
+                
+                int lB = st.empty() ? 0 : st.top()+1;
+                int rB = i < n ? i-1 : n-1;
                 
                 // Calculate area
                 int area = (rB - lB + 1) * heights[idx];
                 res = max(res, area);
-                
-                st.pop();
             }
             
-            // Assign left-boundary
-            if(!st.empty())     leftSmall[i] = st.top()+1;
-            
-            st.push(i);
-        }
-        
-        // Calculate the remaining elements in stack
-        while(!st.empty()) {
-            int idx = st.top();
-            int lB = leftSmall[idx], rB = n-1;
-
-            // Calculate the area
-            int area = (rB - lB + 1) * heights[idx];
-            res = max(res, area);
-            
-            st.pop();
+            if(i < n)   st.push(i++);
         }
         
         return res;
