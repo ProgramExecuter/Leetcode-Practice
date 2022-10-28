1358a5066036a8113c659ff48bb0bc9c576c3da7
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2022-10-28 20:47:11 +0530

    Time: 112 ms (38.23%), Space: 13.3 MB (64.01%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size(), res = 0, i = 0;
+        
+        stack<int> st;
+        
+        while(i < n || !st.empty()) {
+            // Calculate area for greater elements and remove them from stack
+            while(
+                !st.empty() &&
+                ((i < n && heights[st.top()] > heights[i]) ||
+                (i == n && !st.empty()))
+            ) {
+                int idx = st.top();
+                st.pop();
+                
+                int lB = st.empty() ? 0 : st.top()+1;
+                int rB = i < n ? i-1 : n-1;
+                
+                // Calculate area
+                int area = (rB - lB + 1) * heights[idx];
+                res = max(res, area);
+            }
+            
+            if(i < n)   st.push(i++);
+        }
+        
+        return res;
+    }
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int nRow = matrix.size(), nCol = matrix[0].size(), ans = 0;
+        vector<int> height(nCol, 0);
+        
+        for(int row = 0; row < nRow; ++row) {
+            // Check for each row
+            for(int col = 0; col < nCol; ++col) {
+                // Increase or assign height
+                if(matrix[row][col] == '1')     ++height[col];
+                else                            height[col] = 0;
+            }
+            
+            // Get rectangle with maximum area
+            ans = max(ans, largestRectangleArea(height));
+        }
+        
+        return ans;
+    }
+};
