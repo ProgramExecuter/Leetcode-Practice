5f8649d06f57074b5dbcc41fb0739e372ed597ca
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2022-10-29 21:31:16 +0530

    Time: 943 ms (5.02%), Space: 61.2 MB (91.64%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0], fast = nums[nums[0]];
+        
+        // Find the loop start, using rabbit and hare method
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            cout << slow << " " << fast << endl;
+        }
+        
+        fast = 0;
+        
+        // Find the duplicate number now
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        return slow;
+    }
+};
