4e925a3517ab5554186d2e78344b8d6d21a13da1
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2022-11-08 00:23:19 +0530

    Time: 17 ms (77.49%), Space: 10.9 MB (82.82%) - LeetHub

--- 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.cpp
+++ 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.cpp
@@ -4,25 +4,33 @@ public:
         vector<string> res;
         multiset<pair<string, string>> st;
         
+        // Iterate all logs
         for(string s : logs) {
+            // Split the identifier and string
             int i = 0, n = s.size();
             while(s[i] != ' ')
                 ++i;
             
+            // Mark them as 'key' and 'val'
             string key = s.substr(0, i);
             string val = s.substr(i+1, n-i-1);
             
+            // If this is numerical string
             if(val[0] >= '0'  and  val[0] <= '9')
                 res.push_back(key + " " + val);
+            // Else insert it to multi-set, for sorting
             else
                 st.insert({val, key});
         }
         
+        // Reverse the numerical result
         reverse(res.begin(), res.end());
         
+        // Start from back
         for(auto curr = st.rbegin(); curr != st.rend(); ++curr)
             res.push_back(curr->second + " " + curr->first);
         
+        // Reverse again to get result
         reverse(res.begin(), res.end());
         
         return res;
