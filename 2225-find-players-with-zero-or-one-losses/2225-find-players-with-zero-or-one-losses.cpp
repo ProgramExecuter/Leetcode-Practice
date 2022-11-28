b40bfd079a453bdaea36057b892cd6a1596ebb7f
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2022-11-28 22:21:11 +0530

    Time: 2014 ms (5.07%), Space: 205.9 MB (11.84%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+        // Track all the players
         set<int> allPlayers;
+        
+        // Track the loss count of each player
         map<int, int> playerLossCnt;
         
+        // Check for each match
         for(auto match : matches) {
+            // Store each player
             allPlayers.insert(match[0]);
             allPlayers.insert(match[1]);
             
+            // Track the cnt of loss
             playerLossCnt[match[1]] += 1;
         }
         
         vector<int> noLossPlayers, oneLossPlayers;
+        
+        // Check for all players (in ascending order)
         for(auto player : allPlayers) {
-            if(playerLossCnt.find(player) == playerLossCnt.end()) {
+            // This player never lost
+            if(playerLossCnt.find(player) == playerLossCnt.end())
                 noLossPlayers.push_back(player);
-            }
-            else if(playerLossCnt[player] == 1) {
+            // This player lost exactly once
+            else if(playerLossCnt[player] == 1)
                 oneLossPlayers.push_back(player);
-            }
         }
         
         return {noLossPlayers, oneLossPlayers};
