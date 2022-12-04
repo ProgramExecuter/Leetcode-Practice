81b2bfacc5336d84cff030fb88258df4879c0f40
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2022-12-04 13:37:24 +0530

    Time: 319 ms (37.66%), Space: 78.3 MB (76.81%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.cpp
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -3,33 +3,23 @@ public:
     int minimumAverageDifference(vector<int>& nums) {
         int n = nums.size();
         
-        // Single element, 0 is avg
-        if(n == 1)      return 0;
+        long long sumStart = 0, sumEnd = 0;
         
-        long long sum = 0;
-        vector<long long> front(n, 0), back(n, 0);
-
-        // Calculate the sum of first 'i+1' elements and store the result
-        sum = 0;
-        for(int i = 0; i < n; ++i) {
-            sum += nums[i];
-            front[i] = sum;
-        }
-        
-        // Calculate the sum of first 'n-i-1' elements and store the result
-        sum = 0;
-        for(int i = n-1; i >= 0; --i) {
-            sum += nums[i];
-            back[i] = sum;
-        }
+        // Store the sum of elements in 'sumEnd' at start
+        // then subtract each element to get the sum of last 'n-i-1'
+        for(int num : nums)     sumEnd += num;
         
         int resIdx = -1, mnDiff = INT_MAX;
-        for(int i = 0; i < n-1; ++i) {
-            // Calculate left and right Average
-            int leftAvg  = front[i]  / (i + 1);
-            int rightAvg = back[i+1] / (n - i - 1);
+        for(int i = 0; i < n; ++i) {
+            // Calculte sum of first 'i+1' elements
+            sumStart += nums[i];
+            int fromStart = sumStart / (i+1);
             
-            int absDiff = abs(leftAvg - rightAvg);
+            // Calculate sum of last 'n-i-1' elements
+            sumEnd -= nums[i];
+            int fromEnd = (i == n-1) ? 0 : ( sumEnd / (n-i-1) );
+            
+            int absDiff = abs(fromStart - fromEnd);
             
             // Check is this is new lowest average diff.
             if(absDiff < mnDiff) {
@@ -37,10 +27,6 @@ public:
                 mnDiff = absDiff;
             }
         }
-        
-        // Last Idx
-        if(front[n-1] / n  <  mnDiff)
-            resIdx = n-1;
             
         return resIdx;
     }
