e2b9ddc176b662d842184aec64346341b3660004
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2022-12-15 21:02:25 +0530

    Time: 29 ms (71.73%), Space: 12.9 MB (59.71%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,33 +1,41 @@
 class Solution {
 public:
-    int f(int idx1, int idx2, string &text1, string &text2, vector<vector<int>> &dp) {
-        if(idx1 >= text1.size()  or  idx2 >= text2.size())
-            return 0;
-        
-        if(dp[idx1][idx2] != -1)
-            return dp[idx1][idx2];
-        
-        // Both Chars Matches
-        if(text1[idx1] == text2[idx2]) {
-            // Increment the length of matches subsequence by 1
-            return dp[idx1][idx2] = 1 + f(idx1+1, idx2+1, text1, text2, dp);
-        }
-        
-        // Both Don't Match
-        else {
-            // We can either move first or second index ahead,
-            // and take max of their results
-            int ch1 = f(idx1, idx2+1, text1, text2, dp);
-            int ch2 = f(idx1+1, idx2, text1, text2, dp);
-            
-            return dp[idx1][idx2] = max(ch1, ch2);
-        }
-    }
     int longestCommonSubsequence(string text1, string text2) {
         int len1 = text1.size(), len2 = text2.size();
         
-        vector<vector<int>> dp(len1, vector<int>(len2, -1));
+        vector<vector<int>> dp(len1+1, vector<int>(len2+1, 0));
         
-        return f(0, 0, text1, text2, dp);
+        for(int idx1 = len1-1; idx1 >= 0; --idx1) {
+            for(int idx2 = len2-1; idx2 >= 0; --idx2) {
+                // Both Chars Matches
+                if(text1[idx1] == text2[idx2]) {
+                    // Increment the length of matches subsequence by 1
+                    dp[idx1][idx2] = 1 + dp[idx1+1][idx2+1];
+                }
+                // Chars Don't Match
+                else {
+                    // We can either move first or second index ahead,
+                    // and take max of their results
+                    int ch1 = dp[idx1][idx2+1];
+                    int ch2 = dp[idx1+1][idx2];
+
+                    dp[idx1][idx2] = max(ch1, ch2);
+                }
+            }
+        }
+        
+        return dp[0][0];
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
