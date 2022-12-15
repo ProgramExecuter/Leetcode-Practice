9b024a6a0689ac0596764c3a34a5a68971b5bde2
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2022-12-15 20:58:14 +0530

    Time: 46 ms (49.23%), Space: 13.2 MB (12.25%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        int n1 = text1.size(), n2 = text2.size();
+    int f(int idx1, int idx2, string &text1, string &text2, vector<vector<int>> &dp) {
+        if(idx1 >= text1.size()  or  idx2 >= text2.size())
+            return 0;
         
-        vector<int> prev(n2+1, 0), curr(n2+1, 0);               // Take 'curr' and 'prev'
+        if(dp[idx1][idx2] != -1)
+            return dp[idx1][idx2];
         
-        for(int idx1 = 1; idx1 <= n1; ++idx1) {
-            for(int idx2 = 1; idx2 <= n2; ++idx2) {             // Check for both strings
-                if(text1[idx1-1] == text2[idx2-1])              // If curr char matches, +1
-                    curr[idx2] = 1 + prev[idx2-1];
-                else                                            // Else, max of 2 choices
-                    curr[idx2] = max(prev[idx2], curr[idx2-1]); // whether to move idx1 or idx2
-            }
-            prev = curr;
+        // Both Chars Matches
+        if(text1[idx1] == text2[idx2]) {
+            // Increment the length of matches subsequence by 1
+            return dp[idx1][idx2] = 1 + f(idx1+1, idx2+1, text1, text2, dp);
         }
         
-        return prev[n2];
+        // Both Don't Match
+        else {
+            // We can either move first or second index ahead,
+            // and take max of their results
+            int ch1 = f(idx1, idx2+1, text1, text2, dp);
+            int ch2 = f(idx1+1, idx2, text1, text2, dp);
+            
+            return dp[idx1][idx2] = max(ch1, ch2);
+        }
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        int len1 = text1.size(), len2 = text2.size();
+        
+        vector<vector<int>> dp(len1, vector<int>(len2, -1));
+        
+        return f(0, 0, text1, text2, dp);
     }
 };
