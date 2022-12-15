97765f010610f3c8e056c68fe6a771507ebb1d6b
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2022-12-15 21:08:10 +0530

    Time: 24 ms (78.16%), Space: 12.8 MB (64.70%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -2,44 +2,26 @@ class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int len1 = text1.size(), len2 = text2.size();
-        
         vector<int> next(len2+1, 0);
         
         for(int idx1 = len1-1; idx1 >= 0; --idx1) {
-            vector<int> curr(len2+1, 0);
+            vector<int> curr(len2+1, 0);                        // Temp array for current DP
             
             for(int idx2 = len2-1; idx2 >= 0; --idx2) {
-                // Both Chars Matches
-                if(text1[idx1] == text2[idx2]) {
-                    // Increment the result length of subsequence by 1
-                    curr[idx2] = 1 + next[idx2+1];
+                if(text1[idx1] == text2[idx2]) {                // Both Chars Matches
+                    curr[idx2] = 1 + next[idx2+1];              // Increment by subSeq's length 1
                 }
-                // Chars Don't Match
-                else {
-                    // We can either move first or second index ahead,
-                    // and take max of their results
-                    int ch1 = curr[idx2+1];
-                    int ch2 = next[idx2];
+                else {                                          // Chars Don't Match
+                    int ch1 = curr[idx2+1];                     // We can either move first
+                    int ch2 = next[idx2];                       // or second index ahead
 
-                    curr[idx2] = max(ch1, ch2);
+                    curr[idx2] = max(ch1, ch2);                 // take max of the choices
                 }
             }
             
-            next = curr;
+            next = curr;                                        // copy 'next' to 'curr'
         }
         
         return next[0];
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
