71380827258fc30543056ca1659b1d0b4033f63e
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2022-12-21 18:33:23 +0530

    Time: 515 ms (26.34%), Space: 72.1 MB (20.00%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.cpp
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -14,15 +14,16 @@ public:
         
         // Check for all adjacent(child) node
         for(int child : adj[curr]) {
-            // Check if the child is already present in same group as parent
-            // then the result is FALSE, since we cannot divide into 2 groups
-            if(grp[child] == inGrp)
-                return false;
-            
             // Not visited
             if(grp[child] == -1) {
                 // If any child return FALSE, then return FALSE
                 if(!bfs(child, 1-inGrp, adj, grp))
+                    return false;
+            }
+            else {
+                // Check if the child is already present in same group as parent
+                // then the result is FALSE, since we cannot divide into 2 groups
+                if(grp[child] == inGrp)
                     return false;
             }
         }
