0d45a40cab9af2c85ac6f362addb847f919a6fea
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2022-12-21 18:31:59 +0530

    Time: 599 ms (9.97%), Space: 72 MB (23.49%) - LeetHub

---
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    // Create Adjacency Vector
+    void createAdj(vector<vector<int>> &dislikes, vector<vector<int>> &adj) {
+        for(auto people : dislikes) {
+            adj[people[0]].push_back(people[1]);
+            adj[people[1]].push_back(people[0]);
+        }
+    }
+    bool bfs(int curr, int inGrp, vector<vector<int>> &adj, vector<int> &grp) {
+        // Put the element in required group based on 'inGrp'
+        // property, 1 => in 1st group    2 => in 2nd group
+        grp[curr] = inGrp;
+        
+        // Check for all adjacent(child) node
+        for(int child : adj[curr]) {
+            // Check if the child is already present in same group as parent
+            // then the result is FALSE, since we cannot divide into 2 groups
+            if(grp[child] == inGrp)
+                return false;
+            
+            // Not visited
+            if(grp[child] == -1) {
+                // If any child return FALSE, then return FALSE
+                if(!bfs(child, 1-inGrp, adj, grp))
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+        vector<vector<int>> adj(n+1, vector<int>());
+        createAdj(dislikes, adj);
+        
+        vector<int> grp(n+1, -1);       // Has detail about in which the node is
+        
+        for(int i = 1; i <= n; ++i) {
+            // Already check this component
+            if(grp[i] != -1)    continue;
+            
+            if(!bfs(i, 0, adj, grp))
+                return false;
+        }
+        
+        return true;
+    }
+};
