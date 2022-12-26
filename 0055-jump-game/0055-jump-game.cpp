ec365f8fc22807b725624ed1a0371b8d802452ce
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2022-12-26 20:28:12 +0530

    Time: 73 ms (86.56%), Space: 48.4 MB (79.88%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, false);
+        int n = nums.size(), maxIdxReachable = 0;
         
-        // Base case, when reches n-1, we get TRUE
-        dp[n-1] = true;
-        
-        for(int i = n-2; i >= 0; --i) {
-            // Check if in next steps, we get TRUE
-            for(int j = i+1; j <= min(i+nums[i], n-1); ++j) {
-                if(dp[j] == true) {
-                    dp[i] = true;
-                    break;
-                }
-            }
+        // Find the maximum idx we can reach
+        for(int i = 0; i < n-1; ++i) {
+            // We cannot reach this index
+            if(i > maxIdxReachable)
+                return false;
+            maxIdxReachable = max(maxIdxReachable, i + nums[i]);
         }
         
-        return dp[0];
+        // If the maximum index reached is >= n-1, we can reach last index
+        return maxIdxReachable >= n-1;
     }
 };
