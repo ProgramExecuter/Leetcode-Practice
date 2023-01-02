30d95fe4c29132a3fa73bbe754814bb1883ac41d
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-01-02 18:29:03 +0530

    Time: 15 ms (65.91%), Space: 8.1 MB (40.30%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int romanToInt(string s) {
+        // Map the characters with their values
         map<char, int> mp({
             {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}
             , {'C', 100}, {'D', 500}, {'M', 1000} 
         });
         
+        // We keep the previous character's value
         int prev = 0, res = 0;
+        
         for(char ch : s) {
+            // If the previous char's value < curr's val, then it must have been used to reduce
             if(prev < mp[ch])
                 res -= 2 * prev;
+            
             res += mp[ch];
             prev = mp[ch];
         }
