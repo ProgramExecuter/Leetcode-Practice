75da7b129a8770b2f4119a1c302cd3d3ad8cf969
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-01-04 20:15:52 +0530

    Time: 358 ms (53.72%), Space: 104.7 MB (11.98%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     int minimumRounds(vector<int>& tasks) {
-        // Sort the tasks by difficulty
-        sort(tasks.begin(), tasks.end());
-        int prevTask = -1, cnt = 0, reqMinRounds = 0;
+        // Map the task level and its count
+        map<int, int> mp;
         
-        for(int task : tasks) {
-            // This is same level as prevTask, then '++cnt'
-            if(task == prevTask)
-                ++cnt;
-            else {
-                // Previous task level only has 1 task, so we cannot complete it
-                if(cnt == 1) 
-                    return -1;
-                
-                reqMinRounds += cnt%3 ? (cnt/3 + 1) : (cnt/3);
-                cnt = 1;    prevTask = task;
-            }
+        for(int task : tasks)
+            ++mp[task];
+        
+        int reqMinRounds = 0;
+        for(auto i : mp) {
+            // Only appeared 1 time, so not possible to complete
+            if(i.second == 1)   return -1;
+            else                reqMinRounds += (i.second + 2)/3;
         }
-        // Same as in loop condition ( for last tasks )
-        if(cnt == 1)    return -1;
-        else            reqMinRounds += cnt%3 ? (cnt/3 + 1) : (cnt/3);
         
         return reqMinRounds;
     }
