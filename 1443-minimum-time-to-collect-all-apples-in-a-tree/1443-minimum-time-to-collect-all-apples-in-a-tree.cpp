a422ae73a515d1d1619c55eed5a0f108d6917ae1
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-01-11 20:14:16 +0530

    Time: 227 ms (67.11%), Space: 65 MB (37.58%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -1,6 +1,7 @@
 class Solution
 {
 public:
+    // Create the adjacency vector from edges vector
     void createAdj(vector<vector<int>> &edges, vector<vector<int>> &adj)
     {
         for(auto i : edges)
@@ -11,16 +12,23 @@ public:
     }
     int f(int curr, vector<vector<int>> &adj, vector<bool> &hasApple, vector<bool> &vis)
     {
+        // Mark this as visited
         vis[curr] = true;
         
         int totalTime = 0;
         
+        // Visit all the unvisited neighbours, and add their time
         for(int neigh : adj[curr])
         {
             if(vis[neigh])  continue;
             totalTime += f(neigh, adj, hasApple, vis);
         }
         
+        // If the totalTime > 0, that means we found an apple in its branches
+        // OR if this node has an apple, then we found an apple in this node
+        // since we found apple, then we have to come to this branch and leave as well
+        // that accounts for 2 seconds, so we add that to totalTime, else we don't have
+        // visit this branch/node, so no need to add it to time
         if(totalTime > 0  or  hasApple[curr])
             totalTime += 2;
         
@@ -34,6 +42,9 @@ public:
         
         int res = f(0, adj, hasApple, vis);
         
+        // We reduce 2 from result, if any apple is found(res > 0), beacause we have
+        // added 2 seconds extra while returning the result from starting vertex, which
+        // we don't have to, since we are already on that node
         if(res > 0)     res -= 2;
         
         return res;
