d6ea2bd9d8399c7f3b97627a4211d0ab2b16f23d
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-01-12 13:56:07 +0530

    Time: 706 ms (71.85%), Space: 231.1 MB (30.37%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // Create adjacency vector
     void createAdj(vector<vector<int>> &edges, vector<vector<int>> &adj)
     {
         for(auto i : edges)
@@ -10,21 +11,28 @@ public:
     }
     vector<int> f(int curr, string &labels, vector<vector<int>> &adj, vector<bool> &vis, vector<int> &res)
     {
+        // Mark this node as visited
         vis[curr] = true;
         vector<int> charCnt(26, 0);
-        ++charCnt[labels[curr]-'a'];
+        ++charCnt[labels[curr]-'a'];        // Count the current node's label
         
         for(int neigh : adj[curr])
         {
+            // Skip if already visited
             if(vis[neigh])      continue;
-            vector<int> tmp = f(neigh, labels, adj, vis, res);
             
+            // Check for all neighbours, and get their character counts
+            // since they are sub-trees, we add their char's count
+            vector<int> tmp = f(neigh, labels, adj, vis, res);
             for(int i = 0; i < 26; ++i)
                 charCnt[i] += tmp[i];
         }
         
+        // Assign result for this node as, no. of character of this node's label
+        // found in it, and its subtrees
         res[curr] = charCnt[labels[curr]-'a'];
         
+        // Return the character count for current subtree
         return charCnt;
     }
     vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels)
