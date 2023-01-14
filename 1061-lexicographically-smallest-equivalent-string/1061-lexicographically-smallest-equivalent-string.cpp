fcc80ac7f0808b1bdb25f60252ed40438e86db8c
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-01-14 19:43:51 +0530

    Time: 3 ms (79.82%), Space: 6.4 MB (78.90%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
@@ -1,28 +1,56 @@
 class Solution {
 public:
-    vector<int> par;
-    Solution()
-    {
-        par = vector<int>(26, -1);
-    }
-    int find(int x)
-    {
-        if(par[x] == -1)    return x;
-        return par[x] = find(par[x]);
-    }
-    void Union(int x, int y)
-    {
-        x = find(x);    y = find(y);
-        if(x != y)      par[max(x, y)] = min(x, y); 
-    }
     string smallestEquivalentString(string s1, string s2, string baseStr)
     {
-        for(auto i = 0; i < s1.size(); ++i)
-            Union(s1[i]-'a', s2[i]-'a');
+        int n = s1.size();
+        vector<int> par(26, -1);
         
+        for(int i = 0; i < n; ++i)
+        {
+            char ch1 = s1[i], ch2 = s2[i];
+            int low, high;
+            
+            // If both characters are same, skip it
+            if(ch1 == ch2)      continue;
+            
+            // If both the characters don't have any parent
+            if(par[ch1-'a'] == -1  and  par[ch2-'a'] == -1)
+            {
+                low  = ch1-'a';
+                high = ch2-'a';
+            }
+            // If both characters have parent
+            else if(par[ch1-'a'] != -1  and  par[ch2-'a'] != -1)
+            {
+                low  = par[ch1-'a'];
+                high = par[ch2-'a'];
+            }
+            // If one of them has parent and other doesn't
+            else
+            {
+                if(par[ch1-'a'] == -1) {
+                    low  = ch1-'a';
+                    high = par[ch2-'a'];
+                } else {
+                    low  = ch2-'a';
+                    high = par[ch1-'a'];
+                }
+            }
+            
+            // Make the smaller character parent of larger character, and also make it
+            // parent of all character having larger character as parent
+            if(low > high)      swap(high, low);
+                
+            par[high] = low;
+            for(int i = 0; i < 26; ++i)
+                if(par[i] == high)  par[i] = low;
+        }
+        
+        // Create the result from smallest parent of each character
         string res = "";
-        for(auto i = 0; i < baseStr.size(); ++i)
-            res += find(baseStr[i]-'a') + 'a';
+        for(char ch : baseStr)
+            if(par[ch-'a'] == -1)   res += ch;
+            else                    res += char('a'+par[ch-'a']);
         
         return res;
     }
