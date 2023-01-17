3e0ade912db06f5ef16a2eba2a9d814c74fc79ff
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-01-17 21:16:44 +0530

    Time: 61 ms (38.99%), Space: 11 MB (82.03%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -2,39 +2,45 @@ class Solution {
 public:
     int minFlipsMonoIncr(string s) {
         int n = s.size();
-        vector<vector<int>> dp(n+1, vector<int>(2, 0));
         
+        // Used two 1-D vectors for which is equivalent to O(1)
+        // So optimized space from O(N) to O(1)
+        vector<int> next(2, 0), curr(2, 0);
+        
+        // Bottom-Up DP(Iterative approach)
         for(int idx = n-1; idx >= 0; --idx) {
             for(int prevCh = 0; prevCh <= 1; ++prevCh) {
                 int flip = 1e9, noFlip = 1e9;
-
+                
                 if(s[idx] == '0') {
                     // Previous is '1', so we must flip and make this '1'
                     if(prevCh == 1) {
-                        flip = 1 + dp[idx+1][1];
+                        flip = 1 + next[1];
                     }
                     // Previous is '0', so we can flip or not flip it
                     else {
-                        flip = 1 + dp[idx+1][1];
-                        noFlip = dp[idx+1][0];
+                        flip = 1 + next[1];
+                        noFlip = next[0];
                     }
                 }
                 else {
                     // Since the previous char is '1', we must make this '1'
                     if(prevCh == 1) {
-                        noFlip = dp[idx+1][1];
+                        noFlip = next[1];
                     }
                     // Previous char is '0', so we can flip or not flip
                     else {
-                        flip = 1 + dp[idx+1][0];
-                        noFlip = dp[idx+1][1];
+                        flip = 1 + next[0];
+                        noFlip = next[1];
                     }
                 }
                 
-                dp[idx][prevCh] = min(flip, noFlip);
+                curr[prevCh] = min(flip, noFlip);
             }
+            
+            next = curr;
         }
         
-        return dp[0][0];
+        return next[0];
     }
 };
