c4a7d9fdf7438f9b7bd80e880a3ce5b90d986891
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-01-17 21:12:21 +0530

    Time: 293 ms (9.37%), Space: 78.9 MB (8.61%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -1,42 +1,40 @@
 class Solution {
 public:
-    int f(int idx, char prevCh, int &n, string &s, vector<vector<int>> &dp) {
-        if(idx == n)    return 0;
-        
-        if(dp[idx][prevCh-'0'] != -1)
-            return dp[idx][prevCh-'0'];
-        
-        int flip = 1e9, noFlip = 1e9;
-        
-        if(s[idx] == '0') {
-            // Previous is '1', so we must flip and make this '1'
-            if(prevCh == '1') {
-                flip = 1 + f(idx+1, '1', n, s, dp);
-            }
-            // Previous is '0', so we can flip or not flip it
-            else {
-                flip = 1 + f(idx+1, '1', n, s, dp);
-                noFlip = f(idx+1, '0', n, s, dp);
-            }
-        }
-        else {
-            // Since the previous char is '1', we must make this '1'
-            if(prevCh == '1') {
-                noFlip = f(idx+1, '1', n, s, dp);
-            }
-            // Previous char is '0', so we can flip or not flip
-            else {
-                flip = 1 + f(idx+1, '0', n, s, dp);
-                noFlip = f(idx+1, '1', n, s, dp);
-            }
-        }
-
-        return dp[idx][prevCh-'0'] = min(flip, noFlip);
-    }
     int minFlipsMonoIncr(string s) {
         int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
+        vector<vector<int>> dp(n+1, vector<int>(2, 0));
         
-        return f(0, '0', n, s, dp);
+        for(int idx = n-1; idx >= 0; --idx) {
+            for(int prevCh = 0; prevCh <= 1; ++prevCh) {
+                int flip = 1e9, noFlip = 1e9;
+
+                if(s[idx] == '0') {
+                    // Previous is '1', so we must flip and make this '1'
+                    if(prevCh == 1) {
+                        flip = 1 + dp[idx+1][1];
+                    }
+                    // Previous is '0', so we can flip or not flip it
+                    else {
+                        flip = 1 + dp[idx+1][1];
+                        noFlip = dp[idx+1][0];
+                    }
+                }
+                else {
+                    // Since the previous char is '1', we must make this '1'
+                    if(prevCh == 1) {
+                        noFlip = dp[idx+1][1];
+                    }
+                    // Previous char is '0', so we can flip or not flip
+                    else {
+                        flip = 1 + dp[idx+1][0];
+                        noFlip = dp[idx+1][1];
+                    }
+                }
+                
+                dp[idx][prevCh] = min(flip, noFlip);
+            }
+        }
+        
+        return dp[0][0];
     }
 };
