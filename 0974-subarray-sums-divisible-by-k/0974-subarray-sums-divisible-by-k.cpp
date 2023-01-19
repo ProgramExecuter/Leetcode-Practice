126c60999403bc9250dbab63c884e06fe547fd59
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-01-19 17:29:15 +0530

    Time: 87 ms (48.17%), Space: 33 MB (5.58%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -3,14 +3,14 @@ public:
     int subarraysDivByK(vector<int>& nums, int k) {
         int n = nums.size(), sum = 0, cnt = 0;
         vector<int> pre(n, 0);
-        map<int, int> mpRem;
-        mpRem[0] = 1;
+        map<int, int> mpRem;                // To track the count of remainder appearing
+        mpRem[0] = 1;                       // To make sure, we get 1st %k = 0
         
         for(int i = 0; i < n; ++i) {
             sum += nums[i];
             int rem = sum % k;
             
-            if(rem < 0)     rem += k;
+            if(rem < 0)     rem += k;       // Make remainder +ve, for ease
             
             cnt += mpRem[rem];
             ++mpRem[rem];
