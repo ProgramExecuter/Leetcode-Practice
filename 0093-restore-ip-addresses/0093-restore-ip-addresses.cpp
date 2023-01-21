768a44a01e7e820fe0a5aeed774be145c9c48a19
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-01-21 13:09:33 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (70.83%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -10,16 +10,21 @@ public:
     vector<string> restoreIpAddresses(string s) {
         vector<string> res;
         
+        // 1st 2nd 3rd sections can be of length 1, 2, 3, so for loop for each one
         for(int a = 1; a < 4; ++a)
         for(int b = 1; b < 4; ++b)
         for(int c = 1; c < 4; ++c)
         {
             int d = s.size() - a - b - c;
+            
+            // Make sure 4th section's length is valid [1,3]
             if(d > 0  and  d < 4)
             {
+                // 4 section's string
                 string aStr = s.substr(0, a), bStr = s.substr(a, b);
                 string cStr = s.substr(a+b, c), dStr = s.substr(a+b+c, d);
                 
+                // 4 sections's integer values
                 int aInt = stoi(aStr), bInt = stoi(bStr);
                 int cInt = stoi(cStr), dInt = stoi(dStr);
                 
@@ -30,6 +35,7 @@ public:
                 )
                     continue;
                 
+                // Create the current IP address
                 string curr = aStr + '.' + bStr + '.' + cStr + '.' + dStr;
                 res.push_back(curr);
             }
