eb0fd650219dbc31b8bcc6f508db641570870d9c
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-01-25 20:18:13 +0530

    Time: 321 ms (60.39%), Space: 101 MB (58.45%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -3,39 +3,41 @@ public:
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
         int n = edges.size();
         
+        // For calculating the distance from node1 & node2
         vector<int> disFrom1(n, INT_MAX), disFrom2(n, INT_MAX);
+        
+        // For making sure we don't end up in a loop
         vector<int> vis(n, false);
         
-        // For node1
-        int currDist = 0, tmpN1 = node1, tmpN2 = node2;
-        while(tmpN1 != -1  and  !vis[tmpN1]) {
-            vis[tmpN1] = true;
-            disFrom1[tmpN1] = currDist;
-            tmpN1 = edges[tmpN1];
+        // Calculate distance from 'node1'
+        int currDist = 0;
+        while(node1 != -1  and  !vis[node1]) {
+            vis[node1] = true;
+            disFrom1[node1] = currDist;
+            node1 = edges[node1];
             ++currDist;
         }
         
         vis.assign(n, false);
         
-        // For node2
+        // Calculate distance from 'node2'
         currDist = 0;
-        while(tmpN2 != -1  and  !vis[tmpN2]) {
-            vis[tmpN2] = true;
-            disFrom2[tmpN2] = currDist;
-            tmpN2 = edges[tmpN2];
+        while(node2 != -1  and  !vis[node2]) {
+            vis[node2] = true;
+            disFrom2[node2] = currDist;
+            node2 = edges[node2];
             ++currDist;
         }
         
+        // Find the result
         int minDist = INT_MAX, resIdx = -1;
         for(int i = 0; i < n; ++i) {
-            // cout << i << " =>  " << disFrom1[i] << "  " << disFrom2[i] << endl;
             int currDist = max(disFrom1[i], disFrom2[i]);
-            // cout << currDist << "->" << minDist << "," << resIdx;
+            
             if(currDist < minDist) {
                 resIdx = i;
                 minDist = currDist;
             }
-            // cout << " -> " << minDist << "," << resIdx << endl;
         }
         
         return resIdx;
