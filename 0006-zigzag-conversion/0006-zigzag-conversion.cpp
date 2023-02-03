88060b37525006a3b1989ca323e81f55898a8051
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-02-03 20:03:43 +0530

    Time: 34 ms (32.89%), Space: 16.5 MB (20.55%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -4,21 +4,24 @@ public:
         vector<vector<char>> tmpRes(nR, vector<char>());
         int nC = s.size(), idx = 0;
         
+        // Iterate the string
         while(idx < nC) {
             int i = 0;
             
+            // Add the normal top-down characters in each row
             while(idx < nC  and  i < nR)
                 tmpRes[i++].push_back(s[idx++]);
             
+            // Add the diagonal(zig-zag) chars starting from 2nd last row to 2nd row 
             i = nR-2;
             while(idx < nC  and  i > 0)
                 tmpRes[i--].push_back(s[idx++]);
         }
         
+        // Create the result
         string res;
         for(auto i : tmpRes)
-            for(auto j : i)
-                res += j;
+            for(auto j : i)     res += j;
         
         return res;
     }
