55b8f60c57344da9b88d031d642a7f7e28fd5616
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-02-03 20:27:52 +0530

    Time: 7 ms (91.77%), Space: 8.4 MB (69.47%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     string convert(string s, int nR) {
+        // If no. of rows === 1, then no need to operate on string, return it
         if(nR == 1)     return s;
+        
         int nC = s.size(), idx = 0;
         
         string res;
+        
         for(int idx = 0; idx < nR; ++idx) {
             int i = idx;
             
@@ -14,11 +17,14 @@ public:
                 
                 // Add the zig-zag char
                 if(idx > 0  and  idx < nR-1) {
+                    // Find & Add the equivalent zig-zag character
                     int currIdx = i + (nR - idx - 1) * 2;
+                    
                     if(currIdx >= 0  and  currIdx < nC)
                         res += s[currIdx];
                 }
                 
+                // Move to next character(s) in this row 
                 i += (2*nR - 2);
             }
         }
