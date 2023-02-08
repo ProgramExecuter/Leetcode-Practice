8dbdb8d15eba4abe393b6be9fc27f3680543e72f
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-02-08 21:18:16 +0530

    Time: 279 ms (37.71%), Space: 17.2 MB (39.26%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
-//     int f(int idx, vector<int> &nums, vector<int> &dp) {
-//         int n = nums.size();
-//         if(idx == n - 1)    return 0;
-//         if(idx >= n)        return 1e8;
-        
-//         if(dp[idx] != -1)
-//             return dp[idx];
-        
-//         int minJump = 1e8;
-        
-//         for(int i = 1; i <= nums[idx]; ++i) {
-//             int currJump = 1;
-//             currJump += f(idx + i, nums, dp);
-//             minJump = min(minJump, currJump);
-//         }
-        
-//         return dp[idx] = minJump;
-//     }
     int jump(vector<int>& nums) {
+        // Create a DP to store min. jumps needed to reach n-1 from each index
         int n = nums.size();
+        
+        // Base case, dp[n-1] = 0, since that is ending position, and we don't need to jump
         vector<int> dp(n, 0);
         
+        // We approach this problem from back, and make our result up, such that we get
+        // min. jumps needed to reach n-1 th index from 0th
         for(int idx = n-2; idx >= 0; --idx) {
             int minJump = 1e8;
         
@@ -31,9 +18,11 @@ public:
                 minJump = min(minJump, currJump);
             }
             
+            // If minJump is >= 1e8, then it means we cannot reach n-1 from this index
             dp[idx] = minJump;
         }
         
+        // Return the min. jumps needed to reach n-1 th index from 0th index
         return dp[0];
     }
 };
