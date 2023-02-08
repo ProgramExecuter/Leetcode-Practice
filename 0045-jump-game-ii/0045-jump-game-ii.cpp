09ce61961e156445750e9dd79d065c66c1b834ec
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-02-08 21:12:58 +0530

    Time: 278 ms (37.80%), Space: 17.3 MB (29.40%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
-    int f(int idx, vector<int> &nums, vector<int> &dp) {
-        int n = nums.size();
-        if(idx == n - 1)    return 0;
-        if(idx >= n)        return 1e8;
+//     int f(int idx, vector<int> &nums, vector<int> &dp) {
+//         int n = nums.size();
+//         if(idx == n - 1)    return 0;
+//         if(idx >= n)        return 1e8;
         
-        if(dp[idx] != -1)
-            return dp[idx];
+//         if(dp[idx] != -1)
+//             return dp[idx];
         
-        int minJump = 1e8;
+//         int minJump = 1e8;
         
-        for(int i = 1; i <= nums[idx]; ++i) {
-            int currJump = 1;
-            currJump += f(idx + i, nums, dp);
-            minJump = min(minJump, currJump);
-        }
+//         for(int i = 1; i <= nums[idx]; ++i) {
+//             int currJump = 1;
+//             currJump += f(idx + i, nums, dp);
+//             minJump = min(minJump, currJump);
+//         }
         
-        return dp[idx] = minJump;
-    }
+//         return dp[idx] = minJump;
+//     }
     int jump(vector<int>& nums) {
         int n = nums.size();
+        vector<int> dp(n, 0);
         
-        vector<int> dp(n, -1);
+        for(int idx = n-2; idx >= 0; --idx) {
+            int minJump = 1e8;
         
-        return f(0, nums, dp);
+            for(int i = 1; i <= min(n-1-idx, nums[idx]); ++i) {
+                int currJump = 1;
+                currJump += dp[idx + i];
+                minJump = min(minJump, currJump);
+            }
+            
+            dp[idx] = minJump;
+        }
+        
+        return dp[0];
     }
 };
