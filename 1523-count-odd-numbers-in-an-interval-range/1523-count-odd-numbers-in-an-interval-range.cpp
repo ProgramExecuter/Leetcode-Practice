c279770107d4feacf4e3463a1b46d957eead2938
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-02-13 20:42:26 +0530

    Time: 3 ms (66.30%), Space: 6 MB (63.61%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -3,6 +3,8 @@ public:
     int countOdds(int low, int high) {
         int nEle = high - low + 1;
         
+        // If there are odd no. of elements in the range, and the range starts from
+        // odd number, then there will be an extra odd element than even
         if(low % 2  and  nEle % 2)
             return (nEle + 1) / 2;
         else
