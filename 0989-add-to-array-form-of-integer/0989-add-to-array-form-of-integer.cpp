1b91bfe4c89202e77a92ca30e2bcddaff76cc2fa
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-02-15 20:47:47 +0530

    Time: 40 ms (28.65%), Space: 28.8 MB (15.11%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
@@ -2,22 +2,21 @@ class Solution {
 public:
     vector<int> addToArrayForm(vector<int>& num, int k) {
         int carry = 0;
+        vector<int> res;
         
-        for(int i = num.size()-1; i >= 0; --i) {
-            int curr = carry + num[i] + k % 10;
-            num[i] = curr % 10;
-            carry = curr / 10;
-            k /= 10;
-        }
-        
-        // If we have carry remaining then add a new digit at the start
-        while(k + carry > 0) {
+        for(int i = num.size()-1; i >= 0  or  carry + k; --i) {
             int curr = carry + k % 10;
-            num.insert(num.begin(), curr % 10);
+            if(i >= 0)  curr += num[i];
+            
+            res.push_back(curr % 10);
+            
             carry = curr / 10;
             k /= 10;
         }
         
-        return num;    
+        // Reverse the array
+        reverse(res.begin(), res.end());
+        
+        return res;    
     }
 };
