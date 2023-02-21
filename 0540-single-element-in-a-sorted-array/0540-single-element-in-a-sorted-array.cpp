6134feede067ede3164f8a8d7952db9b1eea3fa7
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-02-21 21:34:49 +0530

    Time: 51 ms (12.36%), Space: 22.4 MB (16.37%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -4,31 +4,30 @@ public:
         int n = nums.size();
         int low = 0, high = n-1;
         
+        // Binary Search
         while(low < high) {
             int mid = low + (high - low) / 2;
-            cout << low << " " << high << endl;
-//             if(mid == 0) {
+            
+            // Found the unique element
+            if(nums[mid-1] != nums[mid] and nums[mid+1] != nums[mid])
+                return nums[mid];
+            else {
+                // Number of element until 'mid' index
+                int numEle = mid + 1;
                 
-//             }
-//             else if(mid == n-1) {
-                
-//             }
-            // else {
-                if(nums[mid-1] != nums[mid] and nums[mid+1] != nums[mid])
-                    return nums[mid];
-                else {
-                    int numEle = mid + 1;
-                    
-                    if(nums[mid-1] == nums[mid]) {
-                        if(numEle % 2)      high = mid-2;
-                        else                low = mid+1;
-                    }
-                    else {
-                        if(numEle % 2)      low = mid+2;
-                        else                high = mid-1;
-                    }
+                if(nums[mid-1] == nums[mid]) {
+                    // Ex: 1 1 3 5 5 9 9 =>   'mid' is 4
+                    if(numEle % 2)      high = mid-2;
+                    // Ex: 1 1 2 2 3 3   =>   'mid' is 3
+                    else                low = mid+1;
                 }
-            // }
+                else {
+                    // Ex: 1 1 3 3 4 5 5 =>   'mid' is 2
+                    if(numEle % 2)      low = mid+2;
+                    // Ex: 1 1 2 4 4 7 7 =>   'mid' is 3
+                    else                high = mid-1;
+                }
+            }
         }
         
         return nums[low];
