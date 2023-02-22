fdc03d1bfe5f162cf527a23fda26e6ed4bf1fc62
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-02-22 20:39:28 +0530

    Time: 82 ms (26.82%), Space: 26.1 MB (37.41%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -8,13 +8,16 @@ public:
             maxEle = max(maxEle, i);
         }
         
+        // minimum max. limit should be maximum weighted box
         int low = maxEle, high = totWeight;
         
+        // Check using binary search for min. maximum weight limit
         while(low < high) {
             int currMaxWeight = (low + high) / 2;
-            
             int parts = 1, currWeight = 0;
+            
             for(int i = 0; i < weights.size(); ++i) {
+                // We need to make another round
                 if(currWeight + weights[i] > currMaxWeight) {
                     currWeight = 0;
                     ++parts;
@@ -23,8 +26,10 @@ public:
                 currWeight += weights[i];
             }
             
+            // Need higher weight limit
             if(parts > days)
                 low = currMaxWeight + 1;
+            // Try to find lower weight limit, if possible
             else
                 high = currMaxWeight;
         }
