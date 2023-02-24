7697c21896087fd21355c858c832674c196c14cb
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-02-24 21:29:11 +0530

    Time: 553 ms (63.56%), Space: 56.5 MB (98.31%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -2,28 +2,26 @@ class Solution {
 public:
     int minimumDeviation(vector<int>& nums) {
         priority_queue<int> pq; // max heap to store even numbers
-        int m = INT_MAX;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]%2==0){
-                pq.push(nums[i]);
-                m = min(m,nums[i]);
-            }
-            else{
-                pq.push(nums[i]*2);
-                m = min(m,nums[i]*2);
-            }
+        int m = INT_MAX, ans = INT_MAX;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] % 2)     nums[i] *= 2;
+            
+            pq.push(nums[i]);
+            m = min(m, nums[i]);
         }
-        int ans = INT_MAX;
-        while(!pq.empty()){
-            int top = pq.top();
-            pq.pop();
+        
+        while(!pq.empty()) {
+            int top = pq.top();     pq.pop();
+            
             ans = min(ans,top-m);
-            if(top%2!=0){
-                break;
-            }
-            m = min(m,top/2);
-            pq.push(top/2);
+            
+            if(top % 2 != 0)        break;
+            
+            m = min(m, top / 2);
+            pq.push(top / 2);
         }
+        
         return ans;
     }
 };
