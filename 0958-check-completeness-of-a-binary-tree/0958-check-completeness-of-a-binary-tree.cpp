1429bf8b94ad0af00a2ce598601da7bf6acb2185
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-03-15 09:11:25 -0700

    Time: 0 ms (100.00%), Space: 10.3 MB (94.24%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
@@ -12,9 +12,11 @@
 class Solution {
 public:
     bool isCompleteTree(TreeNode* root) {
+        // Queue for level-order traversal
         queue<TreeNode*> q;
         q.push(root);
         
+        // Mark if ended the tree, or end is marked via 'NULL'
         bool ended = false, endMarked = false;
         int level = 0;
         
@@ -27,28 +29,27 @@ public:
                 
                 // Found the end of tree(as it should be)
                 if(!curr) {
-                    // cout << "N " << q.size() << endl;
                     if(q.size() != 0)   return false;
-                    ended = true;
-                    break;
+                    ended = true;       break;
                 }
                 else {
-                    // cout << curr->val << " " << q.size() << endl;
-                    if(curr->left)          q.push(curr->left);
+                    // If child node is found, then push it to queue
+                    // else, put 'NULL' in queue, and mark it as end
+                    if(curr->left)
+                        q.push(curr->left);
                     else {
                         if(!endMarked)      q.push(NULL);
                         endMarked = true;
                     }
 
-                    if(curr->right)         q.push(curr->right);
+                    if(curr->right)
+                        q.push(curr->right);
                     else {
                         if(!endMarked)      q.push(NULL);
                         endMarked = true;
                     }
                 }
             }
-            
-            // cout << level << " , " << len << endl;
             
             if((1 << level) != len  and  !ended)
                 return false;
