be2fff17c837f3801f81ed83c888a36297650bb8
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-09-25 20:27:00 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (25.22%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        vector<int> cnt(26, 0);
+        
+        for(char ch : s)    ++cnt[ch-'a'];
+        for(char ch : t)    --cnt[ch-'a'];
+        
+        for(int i = 0; i < 26; ++i)
+            if(cnt[i] != 0)     return char('a'+i);
+        
+        return 'a';
+    }
+};
