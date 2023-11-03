c2dc78c5b06640c896d5a50ff346ee2c6290c1a5
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-11-03 22:02:17 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (49.67%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -6,11 +6,13 @@ public:
         
         for(int curr = 1, idx = 0; curr <= n and idx < len; ++curr)
         {
+            // If required, we push to stack
             if(curr == target[idx])
             {
                 res.push_back("Push");
                 ++idx;
             }
+            // If not required, then we put it into stack, and pop it out
             else
             {
                 res.push_back("Push");
