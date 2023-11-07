79b00a5f95100e5473a0da9ec7f99de8f84d0785
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-11-07 20:35:12 +0530

    Time: 112 ms (51.20%), Space: 89.3 MB (6.13%) - LeetHub

--- 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.cpp
+++ 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.cpp
@@ -6,10 +6,6 @@ public:
         for(int i = 0; i < dist.size(); ++i)        time.push_back(1.0 * dist[i] / speed[i]);
         sort(time.begin(), time.end());
         
-        for(double curr : time)
-            cout << curr << " ";
-        cout << endl;
-        
         int nEliminated = 0, currTime = 0;
         
         for(double curr : time)
