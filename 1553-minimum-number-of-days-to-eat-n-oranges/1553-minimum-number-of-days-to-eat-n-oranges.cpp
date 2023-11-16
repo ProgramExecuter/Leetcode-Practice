138c15853ed5e086986edd08e24bf6bb2ad12a20
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-11-16 22:06:39 +0530

    Time: 4 ms (91.42%), Space: 9.2 MB (81.19%) - LeetHub

--- 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.cpp
+++ 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.cpp
@@ -1,9 +1,12 @@
 class Solution
 {
 public:
+    map<int, int> dp;
     int minDays(int n) 
     {
         if(n <= 1)  return n;
+        if(dp.find(n) != dp.end())
+            return dp[n];
         
         // Eat n%2 1by1 and n/2 oranges at once
         int mn1 = (n % 2) + minDays(n/2);
@@ -11,6 +14,6 @@ public:
         // Eat n%3 1by1 and 2(n/3) oranges at once
         int mn2 = (n % 3) + minDays(n/3);
         
-        return 1 + min(mn1, mn2);
+        return dp[n] = 1 + min(mn1, mn2);
     }
 };
