97164fceb1a2ef9be24fdbc4a21b1b515e2b2e26
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-11-20 22:23:27 +0530

    Time: 174 ms (43.75%), Space: 102.3 MB (91.88%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -2,12 +2,18 @@ class Solution {
 public:
     int garbageCollection(vector<string>& garbage, vector<int>& travel) 
     {
+        travel.insert(travel.begin(), 0);
+        
         int lastM = -1, lastP = -1, lastG = -1;
-        int len = garbage.size();
+        int len = garbage.size(), res = 0, sum = 0;
         
         for(int i = 0; i < len; ++i)
         {
-            if(garbage[i].find("M") != -1)
+            res += garbage[i].size();       // We will pick all the trash, regardless
+            sum += travel[i];               // Sum the travel time till current index
+            travel[i] = sum;
+            
+            if(garbage[i].find("M") != -1)  // Find last index for each type of garbage
                 lastM = i;
             if(garbage[i].find("P") != -1)
                 lastP = i;
@@ -15,28 +21,11 @@ public:
                 lastG = i;
         }
         
-        int timeM = 0, timeP = 0, timeG = 0;
-        for(int i = 0; i < len; ++i)
-        {
-            string s = garbage[i];
-            
-            if(i <= lastM)
-            {
-                timeM += count(s.begin(), s.end(), 'M');
-                timeM += i == 0 ? 0 : travel[i-1];
-            }
-            if(i <= lastP)
-            {
-                timeP += count(s.begin(), s.end(), 'P');
-                timeP += i == 0 ? 0 : travel[i-1];
-            }
-            if(i <= lastG)
-            {
-                timeG += count(s.begin(), s.end(), 'G');
-                timeG += i == 0 ? 0 : travel[i-1];
-            }
-        }
+        // Add the travel time for different types of garbage(may not travel same distance)
+        if(lastM >= 0)      res += travel[lastM];
+        if(lastP >= 0)      res += travel[lastP];
+        if(lastG >= 0)      res += travel[lastG];
         
-        return timeM + timeP + timeG;
+        return res;
     }
 };
