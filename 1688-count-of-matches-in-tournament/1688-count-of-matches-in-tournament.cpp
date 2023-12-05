d54352e75c5b9a2265cd63314f592b0a01bf07ec
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-12-05 14:54:26 +0530

    Time: 2 ms (44.05%), Space: 6.3 MB (36.15%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -7,10 +7,7 @@ public:
         while(n > 1)
         {
             totalMatches += (n / 2);
-            if(n % 2 == 1)
-                n = (n / 2) + 1;
-            else
-                n /= 2;
+            n = (n+1) / 2;
         }
         
         return totalMatches;
