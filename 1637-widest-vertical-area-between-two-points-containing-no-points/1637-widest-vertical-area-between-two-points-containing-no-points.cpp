34663998442bccff9e666679939d8956c0a6b38a
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-12-21 20:17:41 +0530

    Time: 249 ms (39.03%), Space: 66.6 MB (87.58%) - LeetHub

--- 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.cpp
+++ 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.cpp
@@ -4,9 +4,9 @@ public:
     {
         sort(points.begin(), points.end());
         
-        int mxWidth = 0;
+        int mxWidth = 0, len = points.size();
         
-        for(int i = 1; i < points.size(); ++i)
+        for(int i = 1; i < len; ++i)
             mxWidth = max(mxWidth, (points[i][0] - points[i-1][0]));
         
         return mxWidth;
