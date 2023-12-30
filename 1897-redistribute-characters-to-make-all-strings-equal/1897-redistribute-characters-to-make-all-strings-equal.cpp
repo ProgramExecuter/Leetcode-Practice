8f7cd4bf86ea932e6941ff8b556e3aa67ef80a11
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2023-12-30 18:37:18 +0530

    Time: 12 ms (72.37%), Space: 12.6 MB (35.02%) - LeetHub

--- 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.cpp
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.cpp
@@ -6,14 +6,13 @@ public:
         // , and then each characters's total count should be divisble by N
         // so we can divide the string among each of indexes.
         // N => total no. of words
-        int len = 0;
         vector<int> cntChar(26, 0);
+        int len = words.size();
         
         for(string word : words)
         {
             for(char ch : word)
                 ++cntChar[ch-'a'];
-            ++len;
         }
         
         for(int i = 0; i < 26; ++i)
