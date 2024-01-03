7f2a378f3723b5cd77bd7e613f2dc4bc90ea1814
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2024-01-03 20:27:27 +0530

    Time: 85 ms (76.99%), Space: 29.2 MB (29.62%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        int prevCnt = 0, beamCnt = 0;
+        int prevRowCnt = 0, totalBeams = 0;
         
-        // Check for each row
-        for(int i = 0; i < bank.size(); ++i) {
-            int currCnt = 0;
+        for(string row : bank)
+        {
+            int currRowCnt = 0;
+            for(char ch : row)
+                if(ch == '1')   ++currRowCnt;
             
-            // Count the security devices in this row
-            for(char ch : bank[i])
-                if(ch == '1')   ++currCnt;
+            totalBeams += prevRowCnt * currRowCnt;
             
-            // Now the beamCnt is incremented by (prevCnt*currCnt), since this is no. of
-            // beams coming from this row to some other lower row
-            beamCnt += (prevCnt * currCnt);
-            
-            // If the current count of sec. devices > 0, then it is recorded for the next
-            // row which will have security devices, and we multiply them to get beam cnt
-            if(currCnt > 0)     prevCnt = currCnt;
+            if(currRowCnt > 0)
+                prevRowCnt = currRowCnt;
         }
         
-        // Return the beam count
-        return beamCnt;
+        return totalBeams;
     }
 };
