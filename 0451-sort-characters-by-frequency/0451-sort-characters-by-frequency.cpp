41cf89aef503e3981d53df0d466e56f597a3032f
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2024-02-07 20:32:19 +0530

    Time: 15 ms (25.02%), Space: 11 MB (14.35%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     string frequencySort(string s)
     {
-        map<char, int> charCnt;
-        
+        // Count the chars for each type
+        map<char, int> charCnt;   
         for(char ch : s)
             ++charCnt[ch];
         
+        // Map the count of characters in descding order
         map<int, vector<char>, greater<int>> mp;
-        
         for(auto curr : charCnt)
             mp[curr.second].push_back(curr.first);
         
