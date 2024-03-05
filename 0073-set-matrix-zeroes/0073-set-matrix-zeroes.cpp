a295ece1970cfee4a191ab7c6618fd07ad66a310
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2024-03-05 20:47:01 +0530

    Time: 12 ms (41.52%), Space: 16.3 MB (19.69%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,47 +1,30 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int nRow = matrix.size(), nCol = matrix[0].size();
+        int nRows = matrix.size(), nCols = matrix[0].size();
         
-        int colFlag = 1;
+        // 2 arrays
+        vector<bool> horizontalZero(nCols, false), verticalZero(nRows, false);
         
-        for(int row = 0; row < nRow; ++row) {
-            for(int col = 0; col < nCol; ++col) {
-                if(matrix[row][col] == 0) {
-                    // If its first col
-                    // then we make use of 'colFlag'
-                    if(col == 0) {
-                        // Make first col flag = 0
-                        colFlag = 0;
-                        // Make first element in row = 0
-                        // matrix[row][0] = 0;
-                    }
-                    else {
-                        // Make first element in col = 0
-                        matrix[0][col] = 0;
-                        // Make first element in row = 0
-                        matrix[row][0] = 0;
-                    }
+        for(int row = 0; row < nRows; ++row)
+        {
+            for(int col = 0; col < nCols; ++col)
+            {
+                // Set corresponding flag to zero
+                if(matrix[row][col] == 0)
+                {
+                    horizontalZero[col] = true;
+                    verticalZero[row] = true;
                 }
             }
         }
         
-        // Start from end of matrix to avoid problem
-        // of initializing the flag area of matrix
-        for(int row = nRow-1; row >= 0; --row) {
-            for(int col = nCol-1; col >= 0; --col) {
-                if(col == 0) {
-                    // If row or 'colFlag' = 0
-                    // then make the element 0
-                    if(colFlag == 0  ||  matrix[row][0] == 0)
-                        matrix[row][col] = 0;
-                }
-                else {
-                    // If the row or col flag = 0
-                    // then make the element 0
-                    if(matrix[row][0] == 0  ||  matrix[0][col] == 0)
-                        matrix[row][col] = 0;
-                }
+        for(int row = 0; row < nRows; ++row)
+        {
+            for(int col = 0; col < nCols; ++col)
+            {
+                if(horizontalZero[col]  or  verticalZero[row])
+                    matrix[row][col] = 0;
             }
         }
     }
