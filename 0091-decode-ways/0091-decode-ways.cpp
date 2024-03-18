8317e14d144b5b23d37a9c8524ee539765a8e290
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2024-03-18 20:33:52 +0530

    Time: 4 ms (30.34%), Space: 8.8 MB (36.46%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,14 +1,31 @@
-class Solution {
+class Solution
+{
 public:
-    int numDecodings(string s) {
-        int p = 1, pp, n = s.size();
-        for(int i=n-1;i>=0;i--) 
-        {
-            int cur = s[i]=='0' ? 0 : p;
-            if(i<n-1 && (s[i]=='1'||s[i]=='2'&&s[i+1]<'7')) cur+=pp;
-            pp = p;
-            p = cur;
-        }
-        return s.empty()? 0 : p;   
+    int f(string &s, int idx, vector<int> &dp)
+    {
+        if(idx == s.size())     return 1;       // For 2 Places Selected
+        if(s[idx] == '0')       return 0;       // Cannot start with start '0'
+        if(idx == s.size()-1)   return 1;       // For 1 Place Selected
+        
+        if(dp[idx] != -1)
+            return dp[idx];
+        
+        // Take only one place
+        int ch1 = f(s, idx+1, dp), ch2 = 0;
+        
+        // Take two places
+        int num = stoi(s.substr(idx, 2));
+        
+        if(num >= 1  &&  num <= 26)
+            ch2 = f(s, idx+2, dp);
+        
+        return dp[idx] = ch1 + ch2;
+    }
+    int numDecodings(string s)
+    {
+        int len = s.size();
+        vector<int> dp(len+1, -1);
+        
+        return f(s, 0, dp);
     }
 };
