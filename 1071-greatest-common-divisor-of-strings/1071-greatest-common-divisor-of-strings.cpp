90bdf89bbbbd0ecb0a4017f323438ca453df922b
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2024-04-08 21:13:10 +0530

    Time: 78 ms (5.22%), Space: 44.9 MB (5.02%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,42 +1,54 @@
-class Solution {
+class Solution
+{
 public:
-    string gcdOfStrings(string str1, string str2) {
-        int len1 = str1.size(), len2 = str2.size();
+    bool checkDivisor(string &matching, string &s1, string &s2)
+    {
+        int len = matching.length();
+        int len1 = s1.length(), len2 = s2.length();
+        int idx1 = 0, idx2 = 0;
         
-        string res = "";
-        
-        for(int i = len1; i >= 1; --i) {
-            // If pattern's length can't divide strings without remainder, so no result, continue 
-            if(len1 % i != 0)   continue;
-            if(len2 % i != 0)   continue;
-            
-            // Current pattern to check
-            string patt = str1.substr(0, i);
-            
-            // The string 'str2' is smaller than the pattern, thus impossible
-            if(len2 < i)    continue;
-            
-            bool isValid1 = true, isValid2 = true;
-            
-            // Check if this pattern can be followed in 'str1'
-            for(int j = 0; j < len1; j += i) {
-                string currSub = str1.substr(j, i);
-                if(currSub.compare(patt) != 0)
-                    isValid1 = false;
-            }
-            
-            // Check if this pattern can be followed in 'str2'
-            for(int j = 0; j < len2; j += i) {
-                string currSub = str2.substr(j, i);
-                if(currSub.compare(patt) != 0)
-                    isValid2 = false;
-            }
-            
-            // Is a valid pattern for both strings
-            if(isValid1  and  isValid2)
-                return patt;
+        while(idx1+len <= len1  and  s1.substr(idx1, len) == matching)
+        {
+            idx1 += len;
         }
         
-        return res;
+        while(idx2+len <= len2  and  s2.substr(idx2, len) == matching)
+        {
+            idx2 += len;
+        }
+        
+        return idx1 == len1  and  idx2 == len2;
+    }
+    string gcdOfStrings(string str1, string str2)
+    {
+        int len1 = str1.length(), len2 = str2.length();
+        int idx1 = 0, idx2 = 0;
+        string matching = "";
+        string foundMatch = "";
+        
+        while(idx1 < len1  and  idx2 < len2)
+        {
+            cout << str1[idx1] << " " << str2[idx2] << " Hello \n";
+            if(str1[idx1] == str2[idx2])
+            {
+                matching += str1[idx1];
+                bool isDivisor = checkDivisor(matching, str1, str2);
+                
+                cout << " " << isDivisor << " -- " << matching << endl;
+                
+                ++idx1;     ++idx2;
+                
+                if(isDivisor)
+                    foundMatch = matching;
+                else
+                    continue;
+            }
+            else
+            {
+                break;
+            }
+        }
+        
+        return foundMatch;
     }
 };
