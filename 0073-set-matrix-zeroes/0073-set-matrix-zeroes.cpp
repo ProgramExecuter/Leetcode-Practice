cfb0c1897a7d349e49f1484ae2b53a15e94439ba
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2024-05-28 20:51:07 +0530

    Time: 14 ms (14.85%), Space: 16.6 MB (23.15%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,29 +2,47 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int nRows = matrix.size(), nCols = matrix[0].size();
-        
-        // 2 arrays
-        vector<bool> horizontalZero(nCols, false), verticalZero(nRows, false);
+        bool row0 = true;
         
         for(int row = 0; row < nRows; ++row)
         {
             for(int col = 0; col < nCols; ++col)
             {
-                // Set corresponding flag to zero
-                if(matrix[row][col] == 0)
-                {
-                    horizontalZero[col] = true;
-                    verticalZero[row] = true;
-                }
+                if(matrix[row][col] != 0)
+                    continue;
+                
+                // Make row 0
+                if(row == 0)
+                    row0 = false;
+                else
+                    matrix[row][0] = 0;
+                
+                // Make column 0
+                matrix[0][col] = 0;
             }
         }
         
         for(int row = 0; row < nRows; ++row)
         {
             for(int col = 0; col < nCols; ++col)
-            {
-                if(horizontalZero[col]  or  verticalZero[row])
-                    matrix[row][col] = 0;
+                cout << matrix[row][col];
+            cout << endl;
+        }
+        
+        for(int row = nRows-1; row >= 0; --row)
+        {
+            for(int col = nCols-1; col >= 0; --col)
+            {   
+                if(row == 0)
+                {
+                    if(row0 == false  ||  matrix[0][col] == 0)
+                        matrix[row][col] = 0;
+                }
+                else
+                {
+                    if(matrix[row][0] == 0  ||  matrix[0][col] == 0)
+                        matrix[row][col] = 0;
+                }
             }
         }
     }
