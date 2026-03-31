03edf6531a47f0e9e9162c9518d2371ae35f53cd
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2026-03-31 10:00:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,53 +1,41 @@
 class Solution {
 public:
+    void setNewDir(int &i, int &j, int &minRow, int &maxRow, int &minCol, int &maxCol) {
+        if(i==-1 && j==0) {         // PrevDir = up,  NewDir = right
+            i = 0;  j = 1;
+            ++minCol;
+        } else if(i==0 && j==1) {   // PrevDir = right,  NewDir = down
+            i = 1; j = 0;
+            ++minRow;
+        } else if(i==1 && j==0) {   // PrevDir = down,  NewDir = left
+            i = 0; j = -1;
+            --maxCol;
+        } else {                    // PrevDir = left,  NewDir = up
+            i = -1; j = 0;
+            --maxRow;
+        }
+    }
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        // 'dirArr' for directions row,col additions
-        vector<pair<int,int>> dirArr({{0,1},{1,0},{0,-1},{-1,0}});
-        vector<int> ans;
-        
-        int nRow = matrix.size(), nCol = matrix[0].size();
-        // Starting point and direction
-        int rowSt = 0, rowEn = nRow-1, colSt = 0, colEn = nCol-1;
-        int row = 0, col = 0, dir = 0;
-        
-        while(rowSt <= rowEn  &&  colSt <= colEn) {
-            dir %= 4;
-            
-            // Loop in the given direction
-            while(1) {
-                ans.push_back(matrix[row][col]);
-                
-                int nxtRow = row + dirArr[dir].first;
-                int nxtCol = col + dirArr[dir].second;
-                
-                // If we cannot move to next (row, col), then end this
-                if(nxtRow<rowSt || nxtRow>rowEn || nxtCol<colSt || nxtCol>colEn) {
-                    break;
-                }
-                // Else move to next grid
-                else {
-                    row = nxtRow;       col = nxtCol;
-                }
+        int m = matrix.size(), n = matrix[0].size();
+        vector<int> res;
+        int minRow = 0, minCol = 0, maxRow = m-1, maxCol = n-1;
+        int i = 0, j = 0;       // Starting Index
+
+        // Current Direction
+        int di = 0, dj = 1;       // Left starting direction
+
+        while(i >= minRow && i <= maxRow && j >= minCol && j <= maxCol) {
+            res.push_back(matrix[i][j]);
+
+            // Out of bounds
+            if(i+di < minRow || i+di > maxRow || j+dj < minCol || j+dj > maxCol) {
+                // Change direction
+                setNewDir(di, dj, minRow, maxRow, minCol, maxCol);
             }
 
-            // Shrink and move according to direction
-            if(dir == 0) {
-                ++row;  ++rowSt;
-            }
-            else if(dir == 1) {
-                --col;  --colEn;
-            }
-            else if(dir == 2) {
-                --row;  --rowEn;
-            }
-            else {
-                ++col;  ++colSt;
-            }
-            
-            // Move to next direction
-            ++dir;
+            i += di;    j += dj;
         }
-        
-        return ans;
+
+        return res;
     }
 };
