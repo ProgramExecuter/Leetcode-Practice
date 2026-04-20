3b6d8e5e233334cf7defc35db2a11ba00720eccd
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2026-04-20 07:09:42 +0530

    Time: 0 ms (100%), Space: 9.7 MB (29.96%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> ans;
-        
-        // Create a vector of 1
-        // with increasing no. of elements
-        for(int i=1; i<=numRows; ++i)
-        {
-            vector<int> tmp;
-            for(int j=1; j<= i; ++j) {
-                tmp.push_back(1);
+        vector<vector<int>> res;
+
+        for(int i = 0; i < numRows; ++i) {
+            vector<int> row(i+1, 1);
+            
+            if(i > 1) {
+                for(int j = 1; j < i; ++j) {
+                    row[j] = res[i-1][j-1] + res[i-1][j];
+                }
             }
-            ans.push_back(tmp);
+
+            res.push_back(row);
         }
-        
-        // Start from 2nd row
-        for(int i=2; i<numRows; ++i) {
-            for(int j=1; j<i; ++j) {
-                // current-element  =  upper-left + upper element
-                ans[i][j] = ans[i-1][j-1]+ans[i-1][j];
-            }
-        }
-        
-        return ans;
+
+        return res;
     }
 };
