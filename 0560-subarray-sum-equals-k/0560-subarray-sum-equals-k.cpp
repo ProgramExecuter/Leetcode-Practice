e6be886a69cfabeebc3120e24b8530167e9ce239
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2026-04-20 06:54:04 +0530

    Time: 95 ms (21.93%), Space: 53.9 MB (5.96%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        map<int, int> presumCntMap;
+        int sum = 0, res = 0;
+
+        for(int num : nums) {
+            sum += num;
+
+            if(sum == k) { ++res; }
+            
+            res += presumCntMap[sum-k];
+
+            presumCntMap[sum] += 1;
+        }
+
+        return res;
+    }
+};
