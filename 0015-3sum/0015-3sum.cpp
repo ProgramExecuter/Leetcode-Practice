e731fedb0d06fc1480ed91d947f519c068ce042b
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2026-04-21 07:56:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,16 +2,7 @@ class Solution {
 public:
     void twoSum(vector<int>& nums, int st, int en, vector<vector<int>>& res, int target) {
         while(st < en) {
-            if(nums[st] + nums[en] == target) {
-                res.push_back({-1*target, nums[st], nums[en]});
-                ++st;   --en;
-                while(st < en  &&  nums[st-1] == nums[st]) { ++st; }
-                while(en > st  &&  nums[en] == nums[en+1]) { --en; }
-            } else if(nums[st] + nums[en] < target) {
-                ++st;
-            } else {
-                --en;
-            }
+            
         }
     }
     vector<vector<int>> threeSum(vector<int>& nums) {
@@ -19,9 +10,23 @@ public:
         sort(nums.begin(), nums.end());
         vector<vector<int>> res;
 
-        for(int i = 0; i < len; ++i) {
+        for(int i = 0; i < len-2; ++i) {
             if(i > 0  &&  nums[i] == nums[i-1]) { continue; }
-            twoSum(nums, i+1, len-1, res, 0-nums[i]);
+            
+            int st = i+1, en = len-1, sum = 0;
+            while(st < en) {
+                sum = nums[i] + nums[st] + nums[en];
+                if(sum == 0) {
+                    res.push_back({nums[i], nums[st], nums[en]});
+                    ++st;   --en;
+                    while(st < en  &&  nums[st-1] == nums[st]) { ++st; }
+                    while(en > st  &&  nums[en] == nums[en+1]) { --en; }
+                } else if(nums[i] + nums[st] + nums[en] < 0) {
+                    ++st;
+                } else {
+                    --en;
+                }
+            }
         }
 
         return res;
