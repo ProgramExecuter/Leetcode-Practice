b35b4181f030b36c02e8cb0f5ee5b44a22f8bce5
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2026-04-22 09:19:18 +0530

    Time: 0 ms (100%), Space: 12.4 MB (8.73%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        // Put both at same place for SHELL SORT algo
-        for(int i = 0; i < n; ++i) {
-            nums1[m+i] = nums2[i];
-        }
+        int mIdx = m-1, idx = m+n-1, nIdx = n-1;
 
-        int gap = ceil((m + n) / 2.0), low = 0, high = 0;
-        bool lastGapOnce = true;
-
-        while(lastGapOnce) {
-            if(gap == 1)    lastGapOnce = false;
-            low = 0, high = gap;
-            
-            while(high < m+n) {
-                if(nums1[low] > nums1[high])    swap(nums1[low], nums1[high]);
-                ++low;  ++high;
+        while(nIdx >= 0) {
+            if(mIdx >= 0  &&  nums2[nIdx] < nums1[mIdx]) {
+                nums1[idx] = nums1[mIdx];
+                --mIdx;
+            } else {
+                nums1[idx] = nums2[nIdx];
+                --nIdx;
             }
-
-            gap = ceil(gap / 2.0);
+            --idx;
         }
     }
 };
