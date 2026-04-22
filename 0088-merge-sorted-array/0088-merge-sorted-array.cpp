907745f8b9603ed6a3e7e7d2732c1b56d930918d
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2026-04-22 09:08:03 +0530

    Time: 2 ms (3.11%), Space: 12.4 MB (40.77%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n)
-    {
-        int gap = m + n;
-        
-        for(int i = 0; i < n; ++i)
-        {
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        // Put both at same place for SHELL SORT algo
+        for(int i = 0; i < n; ++i) {
             nums1[m+i] = nums2[i];
         }
-        
-        while(gap > 1)
-        {
-            if(gap % 2)     gap = (gap / 2) + 1;
-            else            gap = gap / 2;
+
+        int gap = ceil((m + n) / 2.0), low = 0, high = 0;
+        bool lastGapOnce = true;
+
+        while(lastGapOnce) {
+            if(gap == 1)    lastGapOnce = false;
+            low = 0, high = gap;
             
-            int frontIdx = 0, backIdx = gap;
-            
-            while(backIdx < m+n)
-            {
-                if(nums1[frontIdx] >= nums1[backIdx])
-                    swap(nums1[frontIdx], nums1[backIdx]);
-                
-                
-                ++frontIdx;
-                ++backIdx;
+            while(high < m+n) {
+                if(nums1[low] > nums1[high])    swap(nums1[low], nums1[high]);
+                ++low;  ++high;
             }
+
+            gap = ceil(gap / 2.0);
         }
     }
 };
