f342c481c1b99f4aed7acfe5364bf8f3a80881fd
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2026-04-23 08:04:16 +0530

    Time: 0 ms (100%), Space: 25.5 MB (97.2%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
@@ -1,33 +1,76 @@
 class Solution {
 public:
-    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) 
-    {
-        long sum = 0, sum2 = 0;
-        int nRow = grid.size(), nCol = grid[0].size();
-        int n = nRow * nCol;
-        
-        for(auto row : grid)
-        {
-            for(int num : row)
-            {
-                sum += num;
-                sum2 += (1ll * num * num);
+    vector<int> mathSolution(vector<vector<int>>& grid) {
+        int n = grid.size();
+
+        // missing - repeating
+        long long m_r = (1ll * (n*n) * (n*n + 1)) / 2;
+        for(auto arr : grid) {
+            for(auto num : arr)     m_r -= num;
+        }
+
+        // missing^2 - repeating^2
+        long long m2_r2 = (1ll * (n*n) * (n*n + 1) * (2*n*n + 1)) / 6;
+        for(auto arr : grid) {
+            for(auto num : arr)     m2_r2 -= num*num;
+        }
+
+        // m2-r2 / m-r => m+r
+        long long mr = m2_r2 / m_r;
+
+        // m-r + m+r => 2m
+        int missingNum = (m_r + mr) / 2;
+        // m+r - (m-r) => 2r
+        int repeatingNum = (mr - m_r) / 2;
+
+        return {repeatingNum, missingNum};
+    }
+    vector<int> xorSolution(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int xorFM = 0;
+
+        // Do xor of both series
+        for(int i = 0; i < n; ++i) {
+            for(int j = 0; j < n; ++j) {
+                xorFM ^= grid[i][j];
+                xorFM ^= (i*n + j + 1);
             }
         }
-        
-        // Subtract from actual nos should have been
-        // X => missing number   Y => repeating number
-        sum -= (1ll * n * (n + 1)) / 2;                     // Y - X
-        sum2 -= (1ll * n * (n + 1) * (2 * n + 1)) / 6;      // Y^2 - X^2
-                                                            // (Y - X) * (Y + X)
-        
-        // Y + X
-        long tmp = sum2 / sum;
-        
-        int x = (tmp - sum) / 2;
-        int y = (tmp + sum) / 2;
-        
-        // Repeating, Missing number
-        return {y, x};
+
+        // Find any/first bit which is set
+        int idx = 0;
+        while(1) {
+            int num = 1 << idx;
+            if(xorFM & num)   break;
+            ++idx;
+        }
+
+        int xor0 = 0, xor1 = 0;
+        for(int i = 0; i < n; ++i) {
+            for(int j = 0; j < n; ++j) {
+                int num1 = grid[i][j];
+                int num2 = i*n + j + 1;
+                int toXor = 1 << idx;
+
+                if(num1 & toXor)    xor1 ^= num1;
+                else                xor0 ^= num1;
+
+                if(num2 & toXor)    xor1 ^= num2;
+                else                xor0 ^= num2;
+            }
+        }
+
+        bool found = any_of(grid.begin(), grid.end(), [&](const vector<int>& row) {
+            return find(row.begin(), row.end(), xor0) != row.end();
+        });
+
+        if(found)
+            return {xor0, xor1};
+        else
+            return {xor1, xor0};
+    }
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        // return mathSolution(grid);
+        return xorSolution(grid);
     }
 };
