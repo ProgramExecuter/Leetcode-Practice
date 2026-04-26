dcc8ca585a6615d2d4fbab726197566d04ca9307
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2026-04-26 09:55:51 +0530

    Time: 694 ms (7.88%), Space: 170.2 MB (27.36%) - LeetHub

---
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int m, n;
+    pair<int,int> startIdx;
+
+    bool dfs(vector<vector<char>>& grid, int i, int j, pair<int,int> prevIdx, set<pair<int, int>>& vis, char prevChar) {
+        vector<pair<int,int>> dirs({{0,1}, {1,0}, {0,-1}, {-1,0}});
+        vis.insert({i,j});
+
+        for(auto dir : dirs) {
+            int newI = i + dir.first, newJ = j + dir.second;
+
+            if(newI < 0  ||  newI >= m  ||  newJ < 0  ||  newJ >= n)
+                continue;
+            if((prevIdx.first == newI && prevIdx.second == newJ) || grid[newI][newJ] != prevChar)
+                continue;
+            if(vis.find({newI,newJ}) != vis.end()  ||  !dfs(grid,newI,newJ,{i,j},vis,grid[i][j]))
+                return false;
+        }
+
+        return true;
+    }
+    bool containsCycle(vector<vector<char>>& grid) {
+        m = grid.size();    n = grid[0].size();
+        set<pair<int,int>> vis;
+
+        for(int i = 0; i < m; ++i) {
+            for(int j = 0; j < n; ++j) {
+                if(vis.find({i,j}) != vis.end())
+                    continue;
+                startIdx = {i,j};
+
+                if(!dfs(grid, i, j, {-1,-1}, vis, grid[i][j]))
+                    return true;
+            }
+        }
+        
+        return false;
+    }
+};
