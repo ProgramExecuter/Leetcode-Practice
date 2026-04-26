16f0b8f2bf88b6112005311225e27586899f9369
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2026-04-26 17:40:54 +0530

    Time: 0 ms (100%), Space: 17.6 MB (18.94%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,45 +1,42 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int low = 0, high = nums.size()-1, mid = 0;
-        int st = high+1, en = -1;
+        int n = nums.size();
+        int low = 0, high = n-1, mid = 0;
+        int firstIdx = n, lastIdx = -1;
         
-        // Find first index of 'target'
-        while(low <= high)
-        {
-            int mid = (low + high) / 2;
-            
-            if(nums[mid] == target)
-            {
-                if(mid < st)    st = mid;
+        // First idx of target
+        while(low <= high) {
+            mid = low + (high-low)/2;
+
+            if(nums[mid] == target) {
+                firstIdx = min(firstIdx, mid);
+                high = mid-1;
+            } else if(nums[mid] < target) {
+                low = mid+1;
+            } else {
                 high = mid-1;
             }
-            else if(nums[mid] < target)
-                low = mid + 1;
-            else
-                high = mid - 1;
         }
-        
-        low = 0, high = nums.size()-1;
-        // Find last index of 'target'
-        while(low <= high)
-        {
-            int mid = (low + high) / 2;
-            
-            if(nums[mid] == target)
-            {
-                if(mid > en)    en = mid;
+
+        low = 0, high = n-1;
+
+        // Last idx of target
+        while(low <= high) {
+            mid = low + (high-low)/2;
+
+            if(nums[mid] == target) {
+                lastIdx = max(lastIdx, mid);
                 low = mid+1;
+            } else if(nums[mid] < target) {
+                low = mid+1;
+            } else {
+                high = mid-1;
             }
-            else if(nums[mid] < target)
-                low = mid + 1;
-            else
-                high = mid - 1;
         }
-        
-        if(en == -1)
-            return {-1,-1};
-        
-        return {st, en};
+
+        if(firstIdx == n)   firstIdx = -1;
+
+        return {firstIdx, lastIdx};
     }
 };
