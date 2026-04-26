98cdef496f88e9608bc8709b44fcc3ae3304dea0
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2026-04-26 17:18:55 +0530

    Time: 648 ms (5.06%), Space: 17.7 MB (59.28%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size(), res = INT_MIN, currProd = 1;
+
+        // Calculate Prefix Product, Suffix Product
+        // While keeping in mind that each zero divides the array into parts
+        for(int i = 0; i < n; ++i) {
+            if(nums[i] == 0) {         // Zero
+                currProd = 1;
+            } else {
+                currProd *= nums[i];
+                res = max(res, currProd);
+            }
+            res = max(res, nums[i]);
+            cout << "Res:" << res << " Idx:" << i << " Curr:" << currProd << " Ele:" << nums[i] << endl;
+        }
+        cout << "First Done\n";
+
+        currProd = 1;
+        for(int i = n-1; i >= 0; --i) {
+            if(nums[i] == 0) {         // Zero
+                currProd = 1;
+            } else {
+                currProd *= nums[i];
+                res = max(res, currProd);
+            }
+            cout << "Res:" << res << " Idx:" << i << " Curr:" << currProd << " Ele:" << nums[i] << endl;
+        }
+
+        return res;
+    }
+};
