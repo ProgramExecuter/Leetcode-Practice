030986db43af31915bfd69f8750a2e972261038d
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2026-04-30 07:15:43 +0530

    Time: 0 ms (100%), Space: 13.4 MB (27.64%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,34 +1,14 @@
 class Solution {
 public:
-    int findKthPositive(vector<int>& arr, int k) {
-        int n = arr.size(), tmpRes = -1, res = -1;
-        int low = 0, high = n-1;
-
-        while(low <= high) {
-            int mid = low + (high-low) / 2;
-            int missingNumsCntTillNow = arr[mid] - (mid+1);
-
-            if(missingNumsCntTillNow >= k) {
-                tmpRes = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
+    int findKthPositive(vector<int>& A, int k) {
+        int l = 0, r = A.size(), m;
+        while (l < r) {
+            m = (l + r) / 2;
+            if (A[m] - 1 - m < k)
+                l = m + 1;
+            else
+                r = m;
         }
-
-        // Now we know the index till which there are at-least k missing numbers
-        if(tmpRes == -1) {
-            int missingTillLastEle = arr[n-1] - n;
-            res = arr[n-1] + (k - missingTillLastEle);
-        } else {
-            if(tmpRes-1 >= 0) {
-                int missingTillPrevNum = arr[tmpRes-1] - tmpRes;
-                res = arr[tmpRes-1] + (k - missingTillPrevNum);
-            } else {
-                res = k;
-            }
-        }
-
-        return res;
+        return l + k;
     }
 };
