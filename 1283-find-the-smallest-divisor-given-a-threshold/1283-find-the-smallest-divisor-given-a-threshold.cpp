aa4ee004722c28892dd5f5d4bda7a6d4edd5ed30
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2026-04-30 06:29:21 +0530

    Time: 15 ms (18.84%), Space: 26.3 MB (14.71%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-    bool check(vector<int>& nums, int divisor, int threshold)
-    {
-        int sum = 0;
-        
-        for(int num : nums)
-            sum += ceil((1.0 * num) / divisor);
-        
-        return sum <= threshold;
-    }
-    int smallestDivisor(vector<int>& nums, int threshold)
-    {
-        int mxNum = INT_MIN;
-        for(int n : nums)   mxNum = max(mxNum, n);
-        
-        int left = 1, right = mxNum;
-        
-        while(left < right)
-        {
-            int mid = left + (right - left) / 2;
-            
-            if(check(nums, mid, threshold))
-                right = mid;
-            else
-                left = mid + 1;
+    int findDividedBySum(vector<int>& nums, int divisor) {
+        int cnt = 0;
+
+        for(int i : nums) {
+            cnt += ceil((1.0*i)/divisor);
         }
-        
-        return left;
+
+        return cnt;
+    }
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int low = 1, high = 1, res = 0;
+        for(int i : nums)   high = max(high, i);
+
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            int divSum = findDividedBySum(nums, mid);
+
+            if(divSum <= threshold) {
+                res = mid;
+                high = mid-1;
+            } else {
+                low = mid+1;
+            }
+        }
+
+        return res;
     }
 };
