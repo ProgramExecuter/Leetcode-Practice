1a9dca5700deda71bea3b0b6a608670863c943d8
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2026-05-10 11:30:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,30 +9,23 @@
  */
 class Solution {
 public:
+    void findParents(TreeNode* root, map<TreeNode*, TreeNode*>& mp) {
+        if(root->left) {
+            mp[root->left] = root;
+            findParents(root->left, mp);
+        }
+        if(root->right) {
+            mp[root->right] = root;
+            findParents(root->right, mp);
+        }
+    }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
         queue<TreeNode*> travQ;
         travQ.push(root);
 
         map<TreeNode*, TreeNode*> nodePar;
         nodePar[root] = NULL;
-
-        while(!travQ.empty()) {
-            int len = travQ.size();
-
-            while(len--) {
-                TreeNode* curr = travQ.front();
-                travQ.pop();
-
-                if(curr->left) {
-                    travQ.push(curr->left);
-                    nodePar[curr->left] = curr;
-                }
-                if(curr->right) {
-                    travQ.push(curr->right);
-                    nodePar[curr->right] = curr;
-                }
-            }
-        }
+        findParents(root, nodePar);
 
         vector<int> res;
         queue<pair<TreeNode*, int>> q;
@@ -50,16 +43,14 @@ public:
             }
 
             if(nodePar[curr.first]  &&  visNodes.count(nodePar[curr.first]) == 0) {
-                q.push({nodePar[curr.first], curr.second+1});
                 visNodes.insert(nodePar[curr.first]);
+                q.push({nodePar[curr.first], curr.second+1});
             }
             if(curr.first->left  &&  visNodes.count(curr.first->left) == 0) {
-                nodePar[curr.first->left] = curr.first;
                 visNodes.insert(curr.first->left);
                 q.push({curr.first->left, curr.second+1});
             }
             if(curr.first->right  &&  visNodes.count(curr.first->right) == 0) {
-                nodePar[curr.first->right] = curr.first;
                 visNodes.insert(curr.first->right);
                 q.push({curr.first->right, curr.second+1});
             }
