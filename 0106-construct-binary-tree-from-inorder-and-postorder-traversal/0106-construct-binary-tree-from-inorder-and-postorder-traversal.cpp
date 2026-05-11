22e4b9ed3cf69581c64a22580290995cfd643536
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2026-05-11 14:07:58 +0530

    Time: 3 ms (71.14%), Space: 27.4 MB (55.73%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,26 +11,26 @@
  */
 class Solution {
 public:
-    TreeNode* solve(vector<int>& in, vector<int>& post, int &postIdx, int low, int high, map<int, int> &mp) {
-        if(low > high  or  postIdx < 0)
-            return NULL;
-        
-        TreeNode* curr = new TreeNode(post[postIdx]);
-        int idxI = mp[post[postIdx]];
-        
-        --postIdx;
-        
-        curr->right = solve(in, post, postIdx, idxI+1, high  , mp);
-        curr->left  = solve(in, post, postIdx, low   , idxI-1, mp);
-        
-        return curr;
+    unordered_map<int,int> valIdx;
+    int n = 0, idxPost = 0;
+    
+    TreeNode* buildNodes(vector<int>& postorder, vector<int>& inorder, int st, int en) {
+        if(st > en || idxPost < 0)      return nullptr;
+
+        TreeNode* root = new TreeNode(postorder[idxPost]);
+        int foundIdx = valIdx[postorder[idxPost--]];
+
+        root->right = buildNodes(postorder, inorder, foundIdx+1, en);
+        root->left  = buildNodes(postorder, inorder, st, foundIdx-1);
+
+        return root;
     }
-    TreeNode* buildTree(vector<int>& in, vector<int>& post) {
-        map<int, int> mp;
-        for(int i = 0; i < in.size(); ++i)
-            mp[in[i]] = i;
-        
-        int postIdx = post.size()-1;
-        return solve(in, post, postIdx, 0, in.size()-1, mp);
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        n = postorder.size();
+        for(int i = 0; i < n; ++i)
+            valIdx[inorder[i]] = i;
+
+        idxPost = n-1;
+        return buildNodes(postorder, inorder, 0, n-1);
     }
 };
