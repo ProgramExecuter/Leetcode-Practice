368a4f5ee0776b7588ef898fb848e1660a495260
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2026-05-26 05:58:46 +0530

    Time: 3 ms (29%), Space: 9.6 MB (27.15%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        vector<bool> hasChar(26*2, false);
-
+        unordered_map<char, bool> charUsed;
+        
+        int cnt = 0;
         for(char ch : word) {
             if(ch >= 'a' && ch <= 'z') {
-                hasChar[ch-'a'] = true;
+                if(charUsed.count(ch) == 0)
+                    charUsed[ch] = false;
+                if(charUsed.count(toupper(ch)) > 0 && !charUsed[toupper(ch)] && !charUsed[ch]) {
+                    ++cnt;
+                    charUsed[ch] = true;
+                    charUsed[toupper(ch)] = true;
+                }
             } else {
-                hasChar[ch-'A' + 26] = true;
+                if(charUsed.count(ch) == 0)
+                    charUsed[ch] = false;
+                if(charUsed.count(tolower(ch)) > 0 && !charUsed[tolower(ch)] && !charUsed[ch]) {
+                    ++cnt;
+                    charUsed[ch] = true;
+                    charUsed[tolower(ch)] = true;
+                }
             }
-        }
-
-        int cnt = 0;
-        for(int i = 0; i < 26; ++i) {
-            if(hasChar[i] && hasChar[26+i])
-                ++cnt;
         }
 
         return cnt;
