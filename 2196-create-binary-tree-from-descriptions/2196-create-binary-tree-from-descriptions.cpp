3d4db1924260a4606bcd3efcad569e5eb6af1655
Amit Singh Rana <59532870+ProgramExecuter@users.noreply.github.com>
2026-06-07 14:29:06 +0530

    Time: 309 ms (15.81%), Space: 281.5 MB (36.61%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -11,47 +11,38 @@
  */
 class Solution {
 public:
-    TreeNode* createBinaryTree(vector<vector<int>>& descriptions)
-    {
-        set<int> isChildSet;
-        map<int, TreeNode*> nodesMap;
-        
-        for(auto currDescription : descriptions)
-        {
-            int parent = currDescription[0];
-            int child = currDescription[1];
-            bool isLeft = currDescription[2];
-            
-            
-            // If nodes not found in map, create new Nodes
-            if(nodesMap.find(parent) == nodesMap.end())
-                nodesMap[parent] = new TreeNode(parent);
-            if(nodesMap.find(child) == nodesMap.end())
-                nodesMap[child] = new TreeNode(child);
-            
-            
-            // Add to set of child nodes
-            isChildSet.insert(child);
-            
-            
-            // Add child node to right side of parent node
-            if(isLeft)
-                nodesMap[parent]->left = nodesMap[child];
-            else
-                nodesMap[parent]->right = nodesMap[child];
-        }
-        
-        // Find the root node
-        TreeNode *rootNode = NULL;
-        for(auto curr : nodesMap)
-        {
-            if(isChildSet.find(curr.first) == isChildSet.end())
-            {
-                rootNode = curr.second;
-                break;
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        // value to  { node, parentNode }
+        map<int, pair<TreeNode*, TreeNode*>> valToNodeAndPar;
+
+        for(auto rel : descriptions) {
+            TreeNode *currPar, *currChild;
+
+            if(valToNodeAndPar.count(rel[0]) == 0) {
+                currPar = new TreeNode(rel[0]);
+                valToNodeAndPar[rel[0]] = {currPar, nullptr};
             }
+            else
+                currPar = valToNodeAndPar[rel[0]].first;
+            
+            if(valToNodeAndPar.count(rel[1]) == 0) {
+                currChild = new TreeNode(rel[1]);
+                valToNodeAndPar[rel[1]] = {currChild, currPar};
+            }
+            else {
+                currChild = valToNodeAndPar[rel[1]].first;
+                valToNodeAndPar[rel[1]].second = currPar;
+            }
+            
+            if(rel[2])      currPar->left  = currChild;
+            else            currPar->right = currChild;
         }
-        
-        return rootNode;
+
+        for(auto childPar : valToNodeAndPar) {
+            if(childPar.second.second == nullptr)
+                return childPar.second.first;
+        }
+
+        return nullptr;
     }
 };
